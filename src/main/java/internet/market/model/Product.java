f083b4ede62d6af7eb3926a6ad3e76ba8fe4eb6d
Marina <maryna.voloshyna.11@gmail.com>
2020-09-04 10:38:24 +0300

    with pluses

--- src/main/java/internet/market/model/Product.java
+++ src/main/java/internet/market/model/Product.java
@@ -36,10 +36,10 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
+        return "Product{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", price=" + price
+                + '}';
     }
 }
